71d6f06fabf9c1a8c9b15774840d930f3f8d99b9
zingp <605156398@qq.com>
2019-12-08 20:32:23 +0800

    map 初始化

---
+++ study/p03map/mapInit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    map<string, int> user_age = {
+        {"liuyouyuan", 23},
+        {"zingp", 25}
+    };  //c++11 支持大括号直接初始化
+
+    // 遍历
+    map<string, int>::iterator iter;
+    for (iter=user_age.begin(); iter!=user_age.end(); iter++) {
+        cout << "name=" << iter->first << " | age=" << iter->second << endl; 
+    };
+    return 0;
+}
+
+//g++ -g -std=c++11 -o mapini mapInit.cpp
