73a85e44d18d34565f5495f1a5af2ba0a4ede12a
zingp <605156398@qq.com>
2020-01-07 23:26:08 +0800

    c++的输入输出

---
+++ study/p02cincout.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+// 要求：提示用户输入一个整数，将该整数分别以8、10、16进制打印打印到屏幕；
+// 要求：提示用户输入一个布尔值，以布尔的方式打印该值到屏幕；
+
+int main(void){
+    cout << "请输入一个整数:" << endl;
+    int x = 0;
+    cin >> x;
+    cout << oct << x << endl;
+    cout << dec << x << endl;
+    cout << hex << x << endl;
+
+    cout << "请输入一个布尔值（0 | 1）" << endl;
+    bool y = false;
+    cin >> y;
+    cout << boolalpha << y << endl;
+
+    system("pause");
+    return 0;
+}
