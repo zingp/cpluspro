08a41adee39e3a1bb352781a679bc48d08d1a7d2
zingp <605156398@qq.com>
2020-01-25 19:29:53 +0800

    指针引用

--- study/part04reference/pointer01.cpp
+++ study/part04reference/pointer01.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 int main()
 {
+    // *就是取值； &就是取地址
     int a = 1;
+    cout << &a <<endl;
     int *p = &a;
     int *&q = p;
 
+
     *q = 10;
+    cout << p <<" "<< *p << endl;
+    cout << q << endl;
+    cout << &q << endl;
+    cout << *&q << endl;
 
     cout << "a=" << a << endl;
     cout << "p=" << *p << endl;
