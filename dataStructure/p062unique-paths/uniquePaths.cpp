7b4f36d8bcbebc6ff8843438ead3bc70280ab0cd
zingp <605156398@qq.com>
2020-02-07 21:25:54 +0800

    不同路径 动态规划解法

---
+++ dataStructure/p062unique-paths/uniquePaths.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) 
+    {
+        int* *dp = new int*[m]();
+        for(int i=0;i<m;i++)
+            dp[i] = new int[n]();
+        for(int x=0;x<m;x++)
+        {
+            for (int y=0;y<n;y++)
+            {
+                if (x==0 || y==0)
+                {
+                    dp[x][y] = 1;
+                }
+                else
+                {
+                    dp[x][y] = dp[x][y-1] + dp[x-1][y];
+                }
+                
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
+
+int main(void)
+{
+    int m=7, n=3;
+    Solution obj;
+    cout << obj.uniquePaths(m, n) << endl;
+    return 0;
+}
