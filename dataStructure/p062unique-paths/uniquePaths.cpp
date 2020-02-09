5c2782d5d8298b901328741ba3d301ace557292c
zingp <605156398@qq.com>
2020-02-09 23:17:21 +0800

    不同路径1

--- dataStructure/p062unique-paths/uniquePaths.cpp
+++ dataStructure/p062unique-paths/uniquePaths.cpp
@@ -3,24 +3,18 @@ using namespace std;
 
 class Solution {
 public:
-    int uniquePaths(int m, int n) 
-    {
+    int uniquePaths(int m, int n) {
         int* *dp = new int*[m]();
         for(int i=0;i<m;i++)
             dp[i] = new int[n]();
-        for(int x=0;x<m;x++)
-        {
-            for (int y=0;y<n;y++)
-            {
-                if (x==0 || y==0)
-                {
+        // int[][] dp = new int[m][n];
+        for(int x=0;x<m;x++) {
+            for (int y=0;y<n;y++) {
+                if (x==0 || y==0) {
                     dp[x][y] = 1;
-                }
-                else
-                {
+                } else {
                     dp[x][y] = dp[x][y-1] + dp[x-1][y];
-                }
-                
+                } 
             }
         }
         return dp[m-1][n-1];
