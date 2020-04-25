34432c5e80225d4abfcd671103e593e30e72ae74
zingp <605156398@qq.com>
2020-04-25 23:07:00 +0800

    hello world

---
+++ study/p001helloword/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // 包含C++标准输入输出的库, 预处理指令
+
+using namespace std;    // 使用命名空间 std，且听下回分解
+
+int main()
+{
+   cout << "Hello C++! " 
+        << "Hello world!" << endl;  // 输出语句；endl表示换行，且刷新缓冲区
+   return 0;
+}
